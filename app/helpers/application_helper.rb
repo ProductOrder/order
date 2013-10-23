be7c0334332a97b78f69faa92b289a592e539bf4
sankettikhande <sanket.tikhande@sidusa.com>
2013-10-23 18:21:57 +0530

    Changes for missing images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+def display_pic(product)
+	puts product.inspect
+	if product.pic.file?   
+		image_tag product.pic.url(:thumb)
+	else    
+		 image_tag('/images/Not_available_icon.jpg',:height=>"90px",:width=>"60px")
+	end 
+end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
 	attr_accessible :name, :price
 	attr_accessible :attach,:pic
 	has_attached_file :pic, :styles => 
-	{ :medium => "300x300>", :thumb => "125x50>" }
+	{ :medium => "300x300>", :thumb => "60x56>" }
 	has_attached_file :attach
+
+
 end
